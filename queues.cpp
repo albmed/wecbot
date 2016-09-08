d9765b337d0e6d85e5e49cdd6a8995db9c6504ce
Albert Medela <albert.medela@gmail.com>
2016-09-08 18:28:54 +0200

    simple producer-consumer example

---
+++ queues.cpp
@@ -0,0 +1,111 @@
+#include <iostream>       // std::cin, std::cout
+#include <queue>          // std::queue
+#include <string>
+#include <boost/thread.hpp>
+#include <boost/thread/scoped_thread.hpp>
+#include <boost/chrono.hpp>
+#include <boost/atomic.hpp>
+
+std::queue<std::string> myqueue;
+boost::atomic<bool> running; 
+boost::mutex m_queueMutex;
+
+// To compile: g++ -g -o queues queues.cpp  -lboost_thread -lboost_system -lboost_chrono 
+
+
+
+/*int main ()
+{
+	std::string data; 
+
+	std::cout << "Please enter some integers (enter 0 to end):\n";
+
+	do {
+		std::cin >> data;
+		myqueue.push (data);
+	} while (data != "0");
+
+	std::cout << "myqueue contains: ";
+	while (!myqueue.empty())
+	{
+		std::cout << ' ' << myqueue.front();
+		myqueue.pop();
+	}
+	std::cout << '\n';
+
+	return 0;
+}*/
+
+void push(std::string& val) { 
+	
+	boost::mutex::scoped_lock lock(m_queueMutex);
+	myqueue.push(val); 
+}
+
+std::string pop() { 
+	std::string val = ""; 
+
+	boost::mutex::scoped_lock lock(m_queueMutex);
+	
+	if (!myqueue.empty()){ 	
+		val = myqueue.front(); 
+		myqueue.pop();
+	}
+		
+	return val; 
+}
+
+
+void wait(int seconds)
+{
+  boost::this_thread::sleep_for(boost::chrono::seconds(seconds));
+}
+
+void consume () { 
+	while (running || !myqueue.empty()) { 
+		if (!myqueue.empty()) { 
+			std::string dt = pop();
+			
+			wait(10); 
+			std::cout << "[CONS] -- Got: " << dt << std::endl; 
+		}
+		wait (1); 
+	}
+	
+	std::cout << "[CONS] -- Quiting.. " << std::endl; 
+}
+
+void produce()
+{
+	std::string data; 
+		
+	do { 
+		std::cout << "# "; 
+		std::cin >> data; 
+		if (data != "0") {
+			push(data); 
+			std::cout << "[PROD] -- Add: " << data << std::endl; 
+		}
+	} while (data != "0"); 
+	
+	std::cout << "[PROD] -- Quiting.. " << std::endl; 
+	
+	running = false; 
+}
+
+int main() {
+	std::cout << "Started... " << std::endl;
+
+	running = true; 
+	
+	boost::thread producer = boost::thread(produce);
+	boost::thread consumer = boost::thread(consume); 
+	//boost::scoped_thread<> producer((boost::thread(produce)));
+	//boost::scoped_thread<> consumer((boost::thread(consume)));
+
+	producer.join(); 
+	consumer.join(); 
+
+	std::cout << "Ended... " << std::endl;
+}
+
